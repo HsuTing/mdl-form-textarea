4050db52e7c27f919a96a77ce0948804298b9da3
HsuTing <hsuting0106@gmail.com>
2016-01-30 19:51:36 +0800

    project init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Textarea for mdl-form
+
+## Parameter
+
+- `style` -> change style
+- `label` -> name of textarea
+- `rows` -> rows of textarea
+- `change`(function) -> you can do something when teatarea is changed. `1st` argument is this component value.
+
+## Example
+
+```
+{   
+  'id': 'component_id',
+  'label': 'Label',
+  'style': {'color': 'red'},
+  'rows': '10'
+}
+```
---
+++ lib/index.jsx
@@ -0,0 +1,43 @@
+'use strict'
+
+import $ from 'jquery';
+import React from 'react';
+
+export default class textarea extends React.Component {
+  render() {
+    let data = this.props.data;
+    this.props.getData(data.id, '');
+    this.props.getComponents(data.id, this);
+
+    return (
+      <div ref="component" className="mdl-textfield mdl-js-textfield" style={data.style}>
+        <textarea className="mdl-textfield__input" type="text" rows={data.rows} id={data.id} onChange={this._onChange.bind(this)}></textarea>
+        <label className="mdl-textfield__label" htmlFor={data.id}>{data.label}</label>
+      </div>
+    );
+  }
+
+  _onChange(e) {
+    let data = this.props.data;
+    let val = $(e.target).val();
+    if(data.change != undefined) {
+      let temp = data.change(val);
+      if(temp != undefined) {
+        val = temp;
+      }
+    }   
+
+    if(data.isNotRequire) {
+      this.props.getData(data.id, val);
+      return;
+    }   
+
+    if($(e.target).val == '') {
+      $(this.refs.component).addClass('is-invalid');
+      this.props.getData(data.id, '');
+    }   
+    else {
+      this.props.getData(data.id, val);
+    }   
+  }
+};
